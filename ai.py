7cb543e1f778be372a021f6885b04fd48fd9c135
Jenesis May <jenesismay@gmail.com>
2026-07-07 13:55:26 -0500

    Add AI model intergration

---
+++ ai.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY")
+)
+
+def ask_ai(prompt):
+    response = client.chat.completions.create(
+        model="qwen/qwen3-30b-a3b",
+        messages=[
+            {
+                "role": "user",
+                "content":prompt
+            }       
+        ]
+    )
+
+    return response.choices[0].message.content
+
+# Test
+answer = ask_ai("Create a one sentence motivation message for a teacher")
+print(answer)
+
